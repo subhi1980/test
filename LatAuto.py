4c4b45df990ca5cbf4759595ce76e1a6c0e38b6e
subhi1980 <44288969+subhi1980@users.noreply.github.com>
2019-03-19 17:34:37 +0000

    Update LatAuto.py

--- LatAuto.py
+++ LatAuto.py
@@ -315,8 +315,8 @@ if __name__ == '__main__':
                                 #print("Water Level % d" % int(distTx))
                                 if blockno % 2==0:
                                         print (blockno)
-                                        time.sleep (random.uniform (0, 9.4))
-                                        #time.sleep (9.4)
+                                        #time.sleep (random.uniform (0, 9.4))
+                                        time.sleep (9.35)
                                         timestamp = int (time.time()*1000.0)
                                         testcontract.transact({'from': web3.eth.coinbase, 'gas': 200000, 'value': int(5)}).SendWL()
                                         #if distTx <= 10:
